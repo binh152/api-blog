35c5fc406b244ee3cec21d434473bfd3a74206b5
binh152 <laolachemchet@gmail.com>
2022-04-07 16:33:52 +0700

    update

--- model/Model.js
+++ model/Model.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-    
-    titleBlg:{
-        type: String,
-        required: true
+const blogSchema = new mongoose.Schema(
+  {
+    titleBlg: {
+      type: String,
+      required: true,
     },
-    story:{
-        type: String,
-        required: false
+    story: {
+      type: String,
+      required: false,
     },
-    image:{
-        type : String,
-        required: false
-    }
-})
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports= mongoose.model('blogSchema',blogSchema)
+module.exports = mongoose.model("blogSchema", blogSchema);
